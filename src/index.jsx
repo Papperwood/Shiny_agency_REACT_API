e03bba83db4a42f20fd46e91630e92876de6b0b7
Papperwood <mauryalexandre.fgmail.com>
2023-05-01 10:31:05 +0200

    refactor: Routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
 import Header from './components/Header'
 import Error from './components/Error'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+
+    body {
+      margin: 0;
+    }
+`
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle />
       <Header />
-      <Switch>
+      <Routes>
         <Route exact path="/">
           <Home />
         </Route>
@@ -28,7 +40,7 @@ ReactDOM.render(
         <Route>
           <Error />
         </Route>
-      </Switch>
+      </Routes>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
