b264a247d88789e4683b30b3ee6c4c916b3f52dd
Papperwood <mauryalexandre.fgmail.com>
2023-05-01 22:40:22 +0200

    style : end

--- src/index.jsx
+++ src/index.jsx
@@ -6,31 +6,28 @@ import Survey from './pages/Survey'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Error from './components/Error'
-import { createGlobalStyle } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-    * {
-      font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
-
-    body {
-      margin: 0;
-    }
-`
+import GlobalStyle from './utils/style/GlobalStyle'
+import { ThemeProvider, SurveyProvider } from './utils/context'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <GlobalStyle />
-      <Header />
-      <Routes>
-      <Route path="/" exact element={<Home />} />
-        <Route path="/Freelances" element={<Freelances />} />
-        <Route path="/Survey" element={<Survey />} />
-        <Route path="/Results" element={<Results />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <ThemeProvider>
+        <SurveyProvider>
+          <GlobalStyle />
+          <Header />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/Survey" element={<Survey />} />
+            <Route path="/Results" element={<Results />} />
+            <Route path="/Freelances" element={<Freelances />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+          <Footer />
+        </SurveyProvider>
+      </ThemeProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
